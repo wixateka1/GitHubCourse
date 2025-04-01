25f574e090489ca5f307319b7847e0fdbafc52df
wixateka1 <93682222+wixateka1@users.noreply.github.com>
2025-04-01 17:48:22 +0200

    Initial commit

---
+++ ci/build-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
+REPO="ghcr.io/$docker_username/"
+echo "${REPO}"
+docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
